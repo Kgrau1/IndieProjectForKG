961408fce43bd68cc658f031bfb5fd6fadc76a68
Kgrau1 <122568541+Kgrau1@users.noreply.github.com>
2023-10-28 15:06:58 -0500

    Made changes to index.jsp to have a seperate page for just user id and added ClockInStatusServlet

---
+++ src/main/java/controller/ClockInStatusServlet.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+@WebServlet(
+        name = "",
+        urlPatterns = { "/" }
+)
+public class ClockInStatusServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String userId = request.getParameter("userId");
+
+        //DAO call
+
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("clockingOptions.jsp");
+        dispatcher.forward(request, response);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,11 +7,10 @@
 </head>
 <body>
     <!-- Add EL to show conformation -->
-    <form action="ClockInServlet" method="post">
+    <form action="ClockInStatusServlet" method="post">
         <label>User ID: </label>
         <input type="text" name="userId">
-        <label>Job: </label>
-        <input type="text" name="jobId">
+        <button type="submit" value="submit">Submit</button>
     </form>
 </body>
 </html>
