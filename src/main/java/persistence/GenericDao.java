07a8c9a20f0c905d464dc7c80f479d7d6e4e2fb5
Kgrau1 <122568541+Kgrau1@users.noreply.github.com>
2023-10-28 19:50:09 -0500

    Added to GenericDao

--- src/main/java/persistence/GenericDao.java
+++ src/main/java/persistence/GenericDao.java
@@ -1,4 +1,36 @@
 package persistence;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+
 public class GenericDao {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public GenericDao() {
+
+    }
+
+    public User getById(int id) {
+        Session session = sessionFactory.openSession();
+        User user = session.get( User.class, id );
+        session.close();
+        return user;
+    }
+
+    public List<> getAll() {
+        Session session = getSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<> query = builder.createQuery(type);
+        Root<> root = query.from();
+        List<> list = session.createQuery(query).getResultList();
+        session.close();
+        return list;
+
+    }
 }
